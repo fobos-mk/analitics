312ad55c1193a493ecd5486e77e12af956150f5d
nnicrow <nnicrow@gmail.com>
2023-03-19 10:32:53 +0300

    reciever message format fix

--- reciever.py
+++ reciever.py
@@ -1,19 +1,18 @@
-import os
 import pika
 import json
 from analys import analitics as anal
 from sender import responce
 
-hostname = "89.108.70.10"    # Ip сервера
-port = 5672                     # Порт
-ReciveQueue = "DownStream"      # Поток получения
-ResponceQueue = "UpStream"      # Поток отправки
+hostname = '89.108.70.10'  # Ip сервера
+port = 5672  # Порт
+ReciveQueue = "DownStream"  # Поток получения
+ResponceQueue = "UpStream"  # Поток отправки
 
 
 def parsing(data):
     print(data)
-    paramArray = []             # Массив параметров
-    IdArray = []                # Массив Id Пользователей
+    paramArray = []  # Массив параметров
+    IdArray = []  # Массив Id Пользователей
     for i in data:
         averageResault = []
         IdArray.append(i["Id"])
@@ -41,19 +40,16 @@ def reciever():
         resaultAnalytics = anal(parsingResult)
         ResponceData = []
         for i in range(len(Id_Array)):
-            ResponceData.append({int(Id_Array[i]): float(resaultAnalytics[i][1])})
-        print(ResponceData)
-        responce(ResponceData, hostname, ResponceQueue)
+            ResponceData.append({"Id": int(Id_Array[i]), "BurnoutPercent" : float(resaultAnalytics[i][1])})
+            #ResponceData.append([int(Id_Array[i]), float(resaultAnalytics[i][1])])
 
-    # parsingResult = parsing(json.loads(body))
-    # resaultAnalytics = anal(parsingResult)
-    # """Нужно вставить обработку обратной структуры"""
-    # responce(parsing(body),hostname, ResponceQueue)
+            # ResponceData.append({int(Id_Array[i]): float(resaultAnalytics[i][1])})
+            print(ResponceData)
+        responce(ResponceData, hostname, ResponceQueue)
 
     channel.basic_consume(queue=ReciveQueue, on_message_callback=callback, auto_ack=True)
 
     print('Ожидание данных.')
     channel.start_consuming()
-
 
 reciever()
